62844d14d5027b73f6c78ac41784653ecaaef931
Gopi Chimata <Gopi.Chimata@safeguardtec.com>
2020-06-17 18:47:33 +0530

    basics and strings test case

--- about_basics.go
+++ about_basics.go
@@ -1,26 +1,27 @@
 package go_koans
 
 func aboutBasics() {
+	var __bool__ = true
 	assert(__bool__ == true)  // what is truth?
 	assert(__bool__ != false) // in it there is nothing false
 
-	var i int = __int__
+	var i int = 1.00000000
 	assert(i == 1.0000000000000000000000000000000000000) // precision is in the eye of the beholder
 
-	k := __int__ //short assignment can be used, as well
+	k := i //short assignment can be used, as well
 	assert(k == 1.0000000000000000000000000000000000000)
 
-	assert(5%2 == __int__)
-	assert(5*2 == __int__)
-	assert(5^2 == __int__)
+	assert(5%2 == 1)
+	assert(5*2 == 10)
+	assert(5^2 == 7)
 
-	var x int
+	var x, __int__ int
 	assert(x == __int__) // zero values are valued in Go
 
-	var f float32
+	var f, __float32__ float32
 	assert(f == __float32__) // for types of all types
 
-	var s string
+	var s, __string__ string
 	assert(s == __string__) // both typical or atypical types
 
 	var c struct {
